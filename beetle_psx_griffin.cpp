dc4424842ce8544ebb04bb88ed8df822b17228fd
twinaphex <libretro@gmail.com>
2014-11-25 09:09:57 +0100

    Don't need FileWrapper any longer

--- beetle_psx_griffin.cpp
+++ beetle_psx_griffin.cpp
@@ -19,7 +19,6 @@
 #include "mednafen/math_ops.cpp"
 #include "mednafen/settings.cpp"
 #include "mednafen/general.cpp"
-#include "mednafen/FileWrapper.cpp"
 #include "mednafen/FileStream.cpp"
 #include "mednafen/MemoryStream.cpp"
 #include "mednafen/Stream.cpp"
