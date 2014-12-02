4c1bb0df6cddc4897ac0dab97da6532040635391
twinaphex <libretro@gmail.com>
2014-12-02 08:24:17 +0100

    GPU.cpp - more documentation

--- mednafen/psx/gpu.cpp
+++ mednafen/psx/gpu.cpp
@@ -2045,47 +2045,47 @@ static void GPU_ProcessFIFO(void)
             cb);
             */
 
-      case 0x20: /* monochrome 3 point polygon */
-      case 0x28: /* monochrome 4 point polygon */
-      case 0x30: /* gradated 3 point polygon */
-      case 0x38: /* gradated 4 point polygon */
+      case 0x20: /* monochrome 3 point polygon      , opaque */
+      case 0x28: /* monochrome 4 point polygon      , opaque */
+      case 0x30: /* gradated 3 point polygon        , opaque */
+      case 0x38: /* gradated 4 point polygon        , opaque */
       case 0x21:
       case 0x29:
       case 0x31:
       case 0x39:
          G_Command_DrawPolygon(3 + ((cc & 0x8) >> 3), ((cc & 0x10) >> 4), 0, -1, 0, 0, MaskEvalAND, CB);
          break;
-      case 0x22:
-      case 0x2A:
-      case 0x32:
-      case 0x3A:
+      case 0x22: /* monochrome 3 point polygon     , semi-transparent */
+      case 0x2A: /* monochrome 4 point polygon     , semi-transparent */
+      case 0x32: /* shaded 3-point polygon         , semi-transparent */
+      case 0x3A: /* shaded 4-point polygon         , semi-transparent */
       case 0x23:
       case 0x2B:
       case 0x33:
       case 0x3b:
          G_Command_DrawPolygon(3 + ((cc & 0x8) >> 3), ((cc & 0x10) >> 4), 0, abr, 0, 0, MaskEvalAND, CB);
          break;
-      case 0x24: 
-      case 0x2C: /* textured 3 point polygon */
-      case 0x34: /* shaded textured 3 point polygon */
-      case 0x3c: /* shaded textured 4 point polygon */
+      case 0x24: /* textured 3-point polygon       , opaque, texture-blending */
+      case 0x2C: /* textured 4 point polygon       , opaque, texture-blending */
+      case 0x34: /* shaded textured 3 point polygon, opaque, texture-blending */
+      case 0x3c: /* shaded textured 4 point polygon, opaque, texture-blending */
          G_Command_DrawPolygon(3 + ((cc & 0x8) >> 3), ((cc & 0x10) >> 4), 1, -1, 1, TexModeLut[TexMode], MaskEvalAND, CB);
          break;
-      case 0x25:
-      case 0x2D:
-      case 0x35:
+      case 0x25: /* textured 3-point polygon       , opaque, raw-texture */
+      case 0x2D: /* textured 4-point polygon       , opaque, raw-texture */
+      case 0x35: 
       case 0x3D:
          G_Command_DrawPolygon(3 + ((cc & 0x8) >> 3), ((cc & 0x10) >> 4), 1, -1, 0, TexModeLut[TexMode], MaskEvalAND, CB);
          break;
-      case 0x26:
-      case 0x2E:
-      case 0x36:
-      case 0x3E:
+      case 0x26: /* textured 3-point polygon       , semi-transparent, texture-blending */
+      case 0x2E: /* textured 4-point polygon       , semi-transparent, texture-blending */
+      case 0x36: /* shaded textured 3-point polygon, semi-transparent, texture-blending */
+      case 0x3E: /* shaded textured 4-point polygon, semi-transparent, texture-blending */ 
          G_Command_DrawPolygon(3 + ((cc & 0x8) >> 3), ((cc & 0x10) >> 4), 1, abr, 1, TexModeLut[TexMode], MaskEvalAND, CB);
          break;
-      case 0x27:
-      case 0x2F:
-      case 0x37:
+      case 0x27: /* textured 3-point polygon       , semi-transparent, raw-texture */
+      case 0x2F: /* textured 4-point polygon       , semi-transparent, raw-texture */
+      case 0x37: 
       case 0x3F:
          G_Command_DrawPolygon(3 + ((cc & 0x8) >> 3), ((cc & 0x10) >> 4), 1, abr, 0, TexModeLut[TexMode], MaskEvalAND, CB);
          break;
@@ -2100,7 +2100,7 @@ static void GPU_ProcessFIFO(void)
             CB);
             */
 
-      case 0x40: /* monochrome line */
+      case 0x40: /* monochrome line                , opaque */
       case 0x41:
       case 0x44:
       case 0x45:
@@ -2128,7 +2128,7 @@ static void GPU_ProcessFIFO(void)
             G_Command_DrawLine_NoShaded(&points[0], k, 0, 0, -1, MaskEvalAND, CB);
          }
          break;
-      case 0x42:
+      case 0x42: /* monochrome line                , semi-transparent */
       case 0x43:
       case 0x46:
       case 0x47:
@@ -2156,7 +2156,7 @@ static void GPU_ProcessFIFO(void)
             G_Command_DrawLine_NoShaded(&points[0], k, 0, 0, abr, MaskEvalAND, CB);
          }
          break;
-      case 0x48: /* single color polyline */
+      case 0x48: /* monochrome polyline             , opaque */
       case 0x49:
       case 0x4C:
       case 0x4D:
@@ -2201,7 +2201,7 @@ static void GPU_ProcessFIFO(void)
             G_Command_DrawLine_NoShaded(&points[0], k, 1, 0, -1, MaskEvalAND, CB);
          }
          break;
-      case 0x4A:
+      case 0x4A: /* monochrome polyline             , semi-transparent */
       case 0x4B:
       case 0x4E:
       case 0x4F:
@@ -2237,7 +2237,7 @@ static void GPU_ProcessFIFO(void)
             G_Command_DrawLine_NoShaded(&points[0], k, 1, 0, abr, MaskEvalAND, CB);
          }
          break;
-      case 0x50: /* gradated line */
+      case 0x50: /* shaded line                     , opaque */
       case 0x51:
       case 0x54:
       case 0x55:
@@ -2257,7 +2257,7 @@ static void GPU_ProcessFIFO(void)
             G_Command_DrawLine_Shaded(&points[0], k, 0, 1, -1, MaskEvalAND, CB);
          }
          break;
-      case 0x52:
+      case 0x52: /* shaded line                     , semi-transparent */
       case 0x53:
       case 0x56:
       case 0x57:
@@ -2284,7 +2284,7 @@ static void GPU_ProcessFIFO(void)
             G_Command_DrawLine_Shaded(&points[0], k, 0, 1, abr, MaskEvalAND, CB);
          }
          break;
-      case 0x58: /* gradated polyline */
+      case 0x58: /* shaded polyline                , opaque */
       case 0x59:
       case 0x5C:
       case 0x5D:
@@ -2332,7 +2332,7 @@ static void GPU_ProcessFIFO(void)
             G_Command_DrawLine_Shaded(&points[0], k, 1, 1, -1, MaskEvalAND, CB);
          }
          break;
-      case 0x5A:
+      case 0x5A: /* shaded polyline                , semi-transparent */
       case 0x5B:
       case 0x5E:
       case 0x5F:
